72b49fe211301c0022c501e1ad85605d0dd5dbb3
bukodi <bukodi@gmail.com>
2017-04-29 11:22:00 +0200

    NodeMCU tests

---
+++ esp8266-lua/script1.lua
@@ -0,0 +1,1 @@
+test contet
---
+++ esp8266-lua/vcc_test.lua
@@ -0,0 +1,17 @@
+print("Vcc test")
+pin=7
+gpio.mode(pin, gpio.OUTPUT)
+--gpio.write(2, gpio.HIGH)
+gpio.write(pin, gpio.LOW)
+
+lighton=0
+tmr.alarm(0,1000,1,function()
+if lighton==0 then 
+    lighton=1 
+    gpio.write(pin, gpio.LOW)
+    -- 512/1024, 50% duty cycle
+else 
+    lighton=0 
+    gpio.write(pin, gpio.HIGH)
+end 
+end)
